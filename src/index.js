14ec871d110899730700d81f93d834cc4de767a6
Kyle Chapman <kyleichapman@gmail.com>
2021-07-01 09:42:26 +0200

    Update EmojiSet description when importing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 /**
- * EmojiSet package for working with emojis.
+ * *EmojiSet* package for working with emojis 😎👀✌️.
  *
- * Provides methods for easy access to emojis and searching for specific emojis.
+ * Provides methods that can get all available emojis, get emojis by their groups
+ * or get emojis by their keywords as well as searching by group or keyword.
+ *
+ * To get started, you can import the package using two methods:
+ * ```js
+ * // ES6 import
+ * import EmojiSet from 'emoji-set'
+ * // or CommonJS import
+ * const EmojiSet = require('emoji-set')
+ * ```
+ *
+ * All the methods are static, so they can all be called directly using the
+ * `EmojiSet.` syntax.
+ *
+ * Enjoy! 🥳🎉
  */
 
 module.exports = require('./EmojiSet')
